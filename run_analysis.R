eca66fed3762bce5b21649d61f6507a9f2d5548d
charicha <chrisrichardson00@gmail.com>
2015-06-21 19:04:15 -0400

    update

--- run_analysis.R
+++ run_analysis.R
@@ -27,6 +27,8 @@ X_train <- read.table("./train/X_train.txt", sep="")
 
 train <- cbind(subject_train, y_train, X_train)
 
+# 4. Appropriately labels the data set with descriptive variable names.
+
 features <- read.table("./features.txt", sep="", as.is=TRUE)
 features <- features[,2]
 trainNames <- c("SubjectID", "activitylabel", features)
@@ -47,9 +49,17 @@ merged <- rbind(train,test)
 
 # 2. Extract only the measurements on the mean and standard deviation for each measurement.
 
-merged_main <- merged[, grep("mean|std", colnames(merged), value=TRUE)]
+merged_main <- cbind(merged[,1:2], merged[, grep("mean|std", colnames(merged), value=TRUE)])
 merged_main <- merged_main[ !duplicated(names(merged_main))]   # to ensure no dupe columns
 
 # 3. Use descriptive activity names to name the activities in the data set.
 
+activity_labels <- read.table("./activity_labels.txt", sep="")
+names(activity_labels) <- c("activitylabel", "activity")
+merged_main_withlabels <- merge(activity_labels, merged_main, by.x="activitylabel", by.y="activitylabel", all=TRUE)
 
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for
+#    each activity and each subject.
+
+x1 <- cbind(merged_main_withlabels[,1:3], 
+            merged_main_withlabels[, grep("mean", colnames(merged_main_withlabels), value=TRUE)])
